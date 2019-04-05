45150ec444b76537a29e13cfebe75aa47af62647
darineben <47186825+darineben@users.noreply.github.com>
2019-04-05 19:00:52 +0200

    Add files via upload

--- js/widget.js
+++ js/widget.js
@@ -20,18 +20,18 @@ class DiapoPhotoModel extends WidgetModel {
 	setUp() {
 		super.setUp();
 		this.imgFolder = "pictures/";
-		this.fileList = ["image1.jpg", "image2.jpg", "image3.jpeg"];
+		this.fileList = ['r.jpeg', 'image3.jpeg', 'image1.jpg', '2.jpeg', 't.jpeg', 'image2.jpg', 'v.jpeg', '5.jpeg', 'p.jpeg', 'images.jpeg', '1.jpeg', '9.jpeg', '0.jpeg', '4.jpeg', 's.jpeg', '8.jpeg']
+;
 	}
 	
-	loadImage(){
-		this.stockerListe = [];
-		for(let i = 0; i < this.fileList.length; i++){
-			let img = HH.create("img");
-			HH.attr(img, {"src" : this.imgFolder + this.fileList[i]});
-			this.stockerListe.push(img);
-		}
-		console.log(this.stockerListe);
-		return this.stockerListe;
+	async loadImage(src){
+		return new Promise((resolve, reject) => {
+			let img = new Image();
+			img.src = src;
+			img.onload = () => resolve(img);
+			img.onerror = reject;
+		});
+
 	}
 
 }
@@ -40,22 +40,65 @@ class DiapoPhotoView extends WidgetView {
 	
 	constructor() {
 		super();
+		this.image = []
+		
 	}
 	
 	setUp() {
 		super.setUp();
+		this.time = 0;
 		
 	}
 
-	draw() {
+	async draw() {
 		super.draw();
 		let i = 0;
-		let width = this.mvc.main.sizeX * 150;
-		let height = this.mvc.main.sizeY * 150;
-		let a = this.mvc.controller.loadImage();
-		a[i].width = width;
-		this.stage.appendChild(a[i]);
+		this.width = this.mvc.main.sizeX * 150;
+		this.height = this.mvc.main.sizeY * 150;
+		this.canvas = HH.create("canvas");
+		HH.attr(this.canvas, {"width" : this.width, "height" : this.height});
+		
+		this.stage.appendChild(this.canvas);
+		this.context = this.canvas.getContext("2d");
+		this.context.rect(50, 50, 100, 100);
+		this.context.stroke();
+		
+		this.image = await (new Promise(async resolve => {
+				let x = await this.afterXms(this.mvc.controller.loadImage());
+				resolve(x);
+			})).catch((e) => {
+				console.log(e);
+			});
+		this.f1();
+		this.update();
+		
 	}
+	
+	async f1(){
+		let x = await this.afterXms(10);
+		console.log(x);
+	}
+	async update(){
+		let i = 0;
+		while(i< Infinity){
+			let img = await this.afterXms(this.image[i % this.image.length], 1000);
+			let f = (img.width > img.height) ? img.width/this.width : img.height/ this.height;
+			let newWidth = img.width/f;
+			let newHeight = img.height/f;
+			this.context.clearRect(0,0,this.width, this.height);
+			this.context.drawImage(img,this.width/2 - newWidth/2,this.height/2 - newHeight/2, newWidth, newHeight);
+			i++;
+		}
+		
+	}
+	
+	afterXms(x, y = 50) {
+		return new Promise (resolve => {
+			setTimeout(() => {
+				resolve(x);
+			}, y);
+		});
+	} 
 
 	
 }
@@ -71,8 +114,13 @@ class DiapoPhotoController extends WidgetController {
 		
 	}
 	
-	loadImage(){
-		return this.mvc.model.loadImage();
+	async loadImage(){
+		let allImg = [];
+		for(let i = 0; i < this.mvc.model.fileList.length; i++){
+			let src = this.mvc.model.imgFolder + this.mvc.model.fileList[i];
+			 this.mvc.model.loadImage(src).then(img => allImg.push(img));
+		}
+		return allImg;
 	}
 	
 }
