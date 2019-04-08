2a850d2c683a44d8ffe2338fc914e10e0032f80d
darineben <47186825+darineben@users.noreply.github.com>
2019-04-08 18:48:52 +0200

    Added  second  transition

--- js/widget.js
+++ js/widget.js
@@ -24,7 +24,7 @@ class DiapoPhotoModel extends WidgetModel {
 ;
 	}
 	
-	async loadImage(src){
+	async loadImage(src){  // charger l'image
 		return new Promise((resolve, reject) => {
 			let img = new Image();
 			img.src = src;
@@ -50,7 +50,7 @@ class DiapoPhotoView extends WidgetView {
 		
 	}
 
-	async draw() {
+	async draw() {  // Affichage
 		super.draw();
 		let i = 0;
 		this.width = this.mvc.main.sizeX * 150;
@@ -69,43 +69,78 @@ class DiapoPhotoView extends WidgetView {
 			})).catch((e) => {
 				console.log(e);
 			});
-		this.f1();
 		this.update();
 		
 	}
 	
-	async f1(){
-		let x = await this.afterXms(10);
-		console.log(x);
-	}
-	async update(){
+	
+	async update(){  // Raffraichit l'affichage
 		let i = 0, j = 0;
+		let one = true;
 		while(i< Infinity){
-			let img = await this.afterXms(this.image[i % this.image.length], 0);
-			let newWidth;
-			let newHeight;
-			if(img.width < this.width || img.height < this.height){
-				let f = await (img.width < img.height) ? this.width/img.width : this.height/img.height;
-				newWidth = img.width*f;
-				newHeight = img.height*f;
-			}else {
-				let f = await (img.width > img.height) ? img.width/this.width : img.height/this.height;
-				newWidth = img.width/f;
-				newHeight = img.height/f;
-			}
-			let 	rect = {x: this.width/2 - newWidth/2, y: this.height/2 - newHeight/2, w: newWidth, y: newHeight};
-			this.buffer.drawImage(img,this.width/2 - newWidth/2,this.height/2 - newHeight/2, newWidth, newHeight);
-			await this.fadeIn(img, rect, j);
+			
+			
 			j += 1;
-			if(j >= 256){
-				let test = await this.afterXms(0, 5000);
-				i++;
-				j = 0;
+			if(one){
+				let img = await this.afterXms(this.image[i % this.image.length], 0);
+				let dim = this.getDimension(img);
+				let rect = this.getRect(img, this.width/2 - dim[0]/2, this.height/2 - dim[1]/2);
+				this.fadeIn(img, rect, j);
+				this.buffer.drawImage(img,this.width/2 - dim[0]/2,this.height/2 - dim[1]/2, dim[0], dim[1]);
+				if(j >= 256){
+					let test = await this.afterXms(0, 5000);
+					i++;
+					j = 0;
+					one = (Math.random() > 0.5)? true: false;
+				}
+			}
+			else{
+				let img = await this.afterXms(this.image[(i-1) % this.image.length], 0);
+				let dim = this.getDimension(img);
+				let rect = this.getRect(img, this.width/2 - dim[0]/2, this.height/2 - dim[1]/2);
+				let img2 = await this.afterXms(this.image[i % this.image.length], 0);
+				let dim2 = this.getDimension(img2);
+				let rect2 = this.getRect(img2, this.width/2 - dim2[0]/2, this.height/2 - dim2[1]/2);
+				this.context.drawImage(img, (rect.x - j),  this.height/2 - dim[1]/2, dim[0], dim[1]);
+				this.context.drawImage(img2, (rect2.x - j) + rect.w,  this.height/2 - dim2[1]/2, dim2[0], dim2[1]);
+				if(j >= dim[0]){
+					let test = await this.afterXms(0, 5000);
+					i++;
+					j = 0;
+					one = (Math.random() > 0.5)? true: false;
+				}
 			}
 		}		
 	}
 	
-	async fadeIn(img, rect, frame){
+	getRect(img, x, y){
+		let dim = this.getDimension(img);
+		let rect = {x: x, y: y, w: dim[0], y: dim[1]};
+		return rect;
+	}
+	
+	getDimension(img){
+		let newWidth;
+		let newHeight;
+		if(img.width < this.width || img.height < this.height){
+			let f = (img.width < img.height && img.width != img.height) ? this.width/img.width : this.height/img.height;
+			newWidth = img.width*f;
+			newHeight = img.height*f;
+		}else {
+			let f = (img.width > img.height) ? img.width/this.width : img.height/this.height;
+			newWidth = img.width/f;
+			newHeight = img.height/f;
+		}
+		return [newWidth, newHeight];
+	}
+	
+	left (img1, img2, rect1, rect2, frame){
+		this.context.drawImage(img1,(rect1.x + frame) % 2 * this.width ,rect1.y, rect1.w, rect1.h);
+		this.context.drawImage(img2,(rect2.x + frame) % 2 * this.width ,rect2.y, rect2.w, rect2.h);
+	}
+	
+	
+	fadeIn(img, rect, frame){ // transition 1 par changement d'opacité
 			this.buffer.drawImage(img,rect.x,rect.y, rect.w, rect.h);
 			let pixels = this.buffer.getImageData(0,0,this.width, this.height);
 			for (let x = 3 ;x < this.width*this.height *4; x+=4){
@@ -114,7 +149,8 @@ class DiapoPhotoView extends WidgetView {
 		   this.context.putImageData(pixels, 0,0);
 	}
 	
-	afterXms(x, y = 50) {
+	
+	async afterXms(x, y = 50) { // fonction d'affection de variables avec un délais
 		return new Promise (resolve => {
 			setTimeout(() => {
 				resolve(x);
